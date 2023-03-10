1dd9e152c69ca2b31a4fe5b3de129f4f4bed9826
elisabetj <116761221+elisabetj@users.noreply.github.com>
2023-03-10 09:15:05 +0000

    Lending claass búinn til

---
+++ src/main/java/is/hi/hbv202g/ass8/Lending.java
@@ -0,0 +1,15 @@
+package is.hi.hbv202g.ass8;
+
+import java.awt.print.Book;
+
+public class Lending {
+
+    public Lending() {
+        Book book;
+        User user;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/is/hi/hbv202g/ass8/LibrarySystem.java
+++ src/main/java/is/hi/hbv202g/ass8/LibrarySystem.java
@@ -4,6 +4,19 @@ package is.hi.hbv202g.ass8;
  * Hello world!
  */
 public class LibrarySystem {
+
+    public void addBookWithTitleAndAuthorlist(String title, List<Author> authors) {
+
+    }
+
+    public void addStudentUser(String name, boolean feePaid) {
+
+    }
+
+    public void addFacultyMemberUser(String name, String department) {
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
